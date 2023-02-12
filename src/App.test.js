33246fe0b3e5443590c986e7f3b30a5e1a58753d
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2023-02-12 16:21:29 -0500

    Added App.test.js to the gitignore because it is currently irrelevant. Also, I just remembered that JetBrains auto-formats the commit messages so any redundant newlines in previous commits stem from habits instilled by VSCode. :>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './thoroughfare';
 
 test('renders learn react link', () => {
-  render(<App />);
+  /*render(<lp />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();*/
 });
