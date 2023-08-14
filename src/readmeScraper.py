f1a6b582c0a92c010d33618d37b3aaf7671725ef
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2023-08-14 15:29:08 -0400

    Switched gears to just passing off the raw html for the README back to the website source.

--- src/readmeScraper.py
+++ src/readmeScraper.py
@@ -11,12 +11,14 @@ def parseTags(toParse: str, parsedTags: list) -> list:
         return parsedTags
     # Implementation:
     firstEndTag = toParse.find(">")
-    if toParse.find(" ") < firstEndTag and toParse[0] == "<":
+    if toParse.find(" ") != -1 and toParse.find(" ") < firstEndTag and toParse[0] == "<":
         tag = toParse[1:toParse.find(" ")]
         parsedTags.append(tag)
         toParse = toParse[firstEndTag+1:]
+    elif toParse.find(" ") == -1 or toParse.find(" ") > toParse.find("/"):
+        toParse = ""
     else:
-        toParse = "" # TODO: deal with different formatting on a given line.
+        toParse = toParse[toParse.find("<"):] # TODO: Specify which tags go with which text (maybe dict?)
     return parseTags(toParse, parsedTags)
 
 def main(args: list):
@@ -38,24 +40,23 @@ def main(args: list):
     page = webdriver.Chrome()                                        # Initializes the webdriver.
     page.get(URL.string)                                             # Gets the page source.
     links = page.find_elements(By.TAG_NAME, "a")                     # Finds all links on the page.
-    for each in links:
-        try: # TODO: create formatted output for when there is no README.(may need a second test repo for related UTs.)
-            if "title=\"README.md\"" in each.get_attribute("outerHTML"):  # If the necessary title is in the link HTML... Note that this will always choose the last README.
-                URL = each.get_attribute("href")                          # Set the URL to the URL for the README.
-        except selenium.common.exceptions.StaleElementReferenceException: # This is an attempt to get around stale element issues by loading the necessary cookies and trying again.
-            if "title=\"README.md\"" in each.get_attribute("outerHTML"):
-                URL = each.get_attribute("href")
+    for each in links: # TODO: create formatted output for when there is no README.(may need a second test repo for related UTs.)
+        if "title=\"README.md\"" in each.get_attribute("outerHTML"):  # If the necessary title is in the link HTML... Note that this will always choose the last README.
+            URL = each.get_attribute("href")                          # Set the URL to the URL for the README.
                                     # ----- Get README Text ----- #
+    # page.get("https://raw.githubusercontent.com/LukasMartini/landing-page/master/README.md") TODO: consider using github API to get formatting information
+    # print(page.page_source)
     page.get(URL)                                                                                                                                                    # Switch webdriver to the new URL
-    text = page.find_elements(By.TAG_NAME, "article")[0].text.split("\n")                                                                                            # Get all the enclosed text and split by newline.
-    tags = page.page_source[page.page_source.find("<article class=\"markdown-body entry-content container-lg\""):page.page_source.find("</article>")+10].split("\n") # Cut the page source down to just the article tags surrounding the README text.
+    # text = page.find_elements(By.TAG_NAME, "article")[0].text.split("\n")                                                                                          # Get all the enclosed text and split by newline.
+    tags = page.page_source[page.page_source.find("<article class=\"markdown-body entry-content container-lg\""):page.page_source.find("</article>")+10]             # Cut the page source down to just the article tags surrounding the README text.
     print(tags)
     # print(text)
+    # TODO: Try just handing off the page source.
                                     # ----- Parse Tags ----- #
-    tags[0] = tags[0][tags[0].find(">")+1:] # Removes the opening article tag from the first tags item, as this is always irrelevant to formatting.
-    for each in tags:
-        tagNames = parseTags(each, [])
-        print(tagNames)
+    # tags[0] = tags[0][tags[0].find(">")+1:] # Removes the opening article tag from the first tags item, as this is always irrelevant to formatting.
+    # for each in tags:
+    #     tagNames = parseTags(each, [])
+    #     print(tagNames)
     # Post-conditions:
     # Return Value:
 
